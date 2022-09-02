42bb82aa49ada3e6c8b3ce4d72da80f000aab83d
Skandesh <skandeshbe@gmail.com>
2022-09-02 17:49:28 +0530

    updated the url for the server

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://noisy-pond-7641.on.fleek.co/",
     credentials: true,
   },
 });
