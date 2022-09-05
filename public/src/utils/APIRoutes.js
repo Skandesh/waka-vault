4d443f3112b053614600752ca4b88c4e70a13996
Skandesh <skandeshbe@gmail.com>
2022-09-05 14:24:02 +0530

    Corrected a typo

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "https://noisy-pond-7641.on.fleek.co/";
+export const host = "https://noisy-pond-7641.on.fleek.co";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
